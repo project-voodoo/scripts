6242e660fd5a43316b6ff23f3228041c6001b313
SIMOND François <francois@lienweb.fr>
2010-10-18 01:23:38 +0200

    test using stock ramdisk as source

--- run_generate_ramdisk.sh
+++ run_generate_ramdisk.sh
@@ -4,7 +4,13 @@ mkdir output 2>/dev/null
 
 rm -rf output/ramdisk-voodoo/*
 
-lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
-	samsung_ramdisks/tmp-ramdisk-source/ \
-	output/ramdisk-voodoo \
-	lagfix/voodoo_ramdisk_parts
+#lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
+#	samsung_ramdisks/tmp-ramdisk-source/ \
+#	output/ramdisk-voodoo \
+#	lagfix/voodoo_ramdisk_parts
+
+# Stock ramdisk
+#lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
+#        samsung_ramdisks/froyo-xxjpm/ \
+#        output/ramdisk-voodoo \
+#        lagfix/voodoo_ramdisk_parts
