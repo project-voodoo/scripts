a3193b9aea29dcb73fe48a4038ab028abca7bb48
SIMOND François <francois@lienweb.fr>
2010-10-21 02:13:24 +0200

    moved to the "working-copy" directory for ramdisks

--- copy_modules_to_ramdisk.sh
+++ copy_modules_to_ramdisk.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "`find linux_gt-i9000/Kernel/ -name '*.ko' -exec cp -v {} samsung_ramdisks/tmp-ramdisk-source/lib/modules \; | wc -l` module(s) copied"
+echo "`find linux_gt-i9000/Kernel/ -name '*.ko' -exec cp -v {} samsung_ramdisks/working-copy/lib/modules \; | wc -l` module(s) copied"
--- run_generate_ramdisk.sh
+++ run_generate_ramdisk.sh
@@ -5,7 +5,7 @@ mkdir output 2>/dev/null
 rm -rf output/ramdisk-voodoo/*
 
 lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
-	samsung_ramdisks/tmp-ramdisk-source/ \
+	samsung_ramdisks/working-copy \
 	output/ramdisk-voodoo \
 	lagfix/voodoo_ramdisk_parts
 
