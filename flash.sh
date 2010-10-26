78908871e923394bb0ac51018d4e60472d4876d0
SIMOND François <francois@lienweb.fr>
2010-10-26 05:59:59 +0200

    busybox not needed anymore

--- flash.sh
+++ flash.sh
@@ -2,5 +2,6 @@
 adb push scripts/redbend_ua /sdcard/
 adb push linux_gt-i9000/zImage /sdcard/
 
-adb shell su -c "/system/xbin/busybox mv /sdcard/redbend_ua /data/"
+adb shell su -c "cat /sdcard/redbend_ua > /data/redbend_ua"
+adb shell su -c "chmod 755 /data/redbend_ua"
 adb shell su -c "/data/redbend_ua restore /sdcard/zImage /dev/block/bml7"
