69774242eecf263d13f1b976668c756ebe7557be
SIMOND François <francois@lienweb.fr>
2010-11-18 23:47:42 +0100

    generate multiboot ramdisk aswell

--- run_generate_ramdisk.sh
+++ run_generate_ramdisk.sh
@@ -2,12 +2,17 @@
 
 mkdir output 2>/dev/null
 
-rm -rf output/ramdisk-voodoo/*
+rm -r output/ramdisk-voodoo/*
 
 lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
 	samsung_ramdisks/working-copy \
 	output/ramdisk-voodoo \
 	lagfix/voodoo_ramdisk_parts
+
+rm -r output/multiboot
+lagfix/multiboot_injector/generate_multiboot_ramdisk.sh \
+	samsung_ramdisks/froyo-i9000xxjpo \
+	output/multiboot
 
 # Stock ramdisk
 #lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
