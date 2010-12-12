90279550aaf41129ef6e555d10fbcc2376fae09d
SIMOND François <francois@lienweb.fr>
2010-12-12 03:46:51 +0100

    clean up

--- run_generate_ramdisk.sh
+++ run_generate_ramdisk.sh
@@ -17,8 +17,3 @@ lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
 #	samsung_ramdisks/froyo-i9000xxjpo \
 #	output/multiboot
 
-# Stock ramdisk
-#lagfix/voodoo_injector/generate_voodoo_ramdisk.sh \
-#        samsung_ramdisks/froyo-xxjpm/ \
-#        output/ramdisk-voodoo \
-#        lagfix/voodoo_ramdisk_parts
