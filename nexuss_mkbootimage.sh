304ae138aedf069a801f9063bf1992f20f7f83a2
SIMOND François <francois@lienweb.fr>
2010-12-17 02:19:15 +0100

    custom script to assemble the Nexus zImage with a custom ramdisk directory

---
+++ nexuss_mkbootimage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -x
+
+# build ramdisk cpio.gz
+cd nexuss-initramfs
+find | grep -v .git | fakeroot cpio -o -H newc | gzip -9 > ../ns_out/initramfs.cpio.gz
+cd ..
+
+mkbootimg \
+	--kernel /home/share/hardware/nexuss/kernel-from-dump/mtdblock2_boot.img-kernel \
+	--ramdisk ns_out/initramfs.cpio.gz \
+	--base 0x30000000 \
+	--pagesize 1000 \
+	-o ns_out/bootimage.img
