037c33a19c963ee7e11f1e724ea412aec017fcdc
SIMOND François <francois@lienweb.fr>
2011-02-27 19:35:27 +0100

    LOCALVERSION for 2.3.3 OTA kernel

--- nexuss_build_kernel.sh
+++ nexuss_build_kernel.sh
@@ -2,7 +2,9 @@
 
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi-
-export LOCALVERSION="-g7f1638a"
+export LOCALVERSION="-g1d030a7"
+
+export KBUILD_BUILD_VERSION="Voodoo"
 
 cd nexuss
 nice -n 10 make -j8
