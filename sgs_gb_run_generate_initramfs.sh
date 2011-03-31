751d181afcb0cba4679b8f12b350c7312c6dc9c5
Her Fritz <ficeto@ficeto.com>
2011-03-31 13:12:59 +0300

    adding JVK initramfs script
    Signed-off-by: Her Fritz <ficeto@ficeto.com>

---
+++ sgs_gb_run_generate_initramfs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p output 2>/dev/null
+
+rm -rf output/voodoo-initramfs/*
+
+lagfix/voodoo_injector/generate_voodoo_initramfs.sh \
+	-s gingerbread-i9000_samsung_initramfs-hacked \
+	-t lagfix/stages_builder/stages/ \
+	-d output/voodoo-initramfs \
+	-c cwm/ \
+	-p lagfix/voodoo_initramfs_parts \
+	-x lagfix/extensions -u -l
+
+#rm -r output/multiboot
+#lagfix/multiboot_injector/generate_multiboot_initramfs.sh \
+#	samsung_initramfs/froyo-i9000xxjpo \
+#	output/multiboot
+
