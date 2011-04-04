60e0937aa0c70a03e03cac5c53fe1845331e5625
Her Fritz <ficeto@ficeto.com>
2011-04-05 01:34:03 +0300

    some script updates
    Signed-off-by: Her Fritz <ficeto@ficeto.com>

--- build_kernel_and_initramfs.sh
+++ build_kernel_and_initramfs.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
-
-# if $1 is not empty update the modules and rebuild zImage with
-# the updated initramfs
-
-
-build_kernel() {
-	cd linux_gt-i9000/
-	test -x build_kernel.sh && bash -c ./build_kernel.sh || ./scripts/build_kernel.sh
-	cd ..
-}
-if test -n "$1"; then
-	scripts/run_generate_initramfs.sh || exit 1
-	scripts/copy_modules_to_initramfs.sh
-	build_kernel
-else
-	scripts/run_generate_initramfs.sh || exit 1
-	build_kernel
-fi
+scripts/run_generate_initramfs.sh || exit 1
+cd linux_gt-i9000/
+./scripts/build_kernel.sh
+cd ..
--- copy_modules_to_initramfs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-find linux_gt-i9000/Kernel/ -name '*.ko' -exec cp -v {} samsung_initramfs/froyo-i9000xxjpy/lib/modules \;
-echo "`find linux_gt-i9000/Kernel/ -name '*.ko' | wc -l` module(s) copied"
